7a170a92dc0bea089e5bcf07567af7bed4a0fe18
Jonathan <jhoo.alves@hotmail.com>
2023-07-17 22:38:36 -0300

    feat: full login logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
+'use client';
+
+import React from "react";
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')
+
+  function handleClick(event) {
+    event.preventDefault()
+    localStorage.setItem('name', name)
+    router.push('/wallet')
+    console.log('clicked')
+    console.log('nome', name);
+  };
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
   return (
     <main>
       <form>
         <label>
           Nome
-          <input type="text" />
-          <button type="submit">Entrar</button>
+          <input type="text" onChange={ handleChange } />
         </label>
+        <button onClick={ handleClick }>Entrar</button>
       </form>
     </main>
   )
-}
+};
--- src/app/wallet/page.js
+++ src/app/wallet/page.js
@@ -3,8 +3,12 @@
 import React from 'react'
 
 function Wallet() {
+  const name = localStorage.getItem('name')
   return (
-    <div>Wallet app</div>
+    <>
+      <div>Wallet app</div>
+      <h1>Olá { name }</h1>
+    </>
   )
 }
 
